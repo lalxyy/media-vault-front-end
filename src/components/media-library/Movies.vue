730100b77f600658bf3e79ec143f367248a277ed
Carl Li <hkyla00@126.com>
2017-06-25 13:42:34 +0800

    example movie

--- src/components/media-library/Movies.vue
+++ src/components/media-library/Movies.vue
@@ -1,37 +1,31 @@
 <template>
   <div>
     <h2>Movies</h2>
-    <el-table :data="filterTableData" stripe style="width: 100%">
+    <el-table :data="tableData" stripe style="width: 100%">
       <!-- Basic (extends from Media) -->
-      <el-table-column prop="title" label="Title" width="300">
-
-      </el-table-column>
-      <el-table-column prop="rating" label="Rating" width="100">
-
-      </el-table-column>
+      <el-table-column prop="title" label="Title" width="200"></el-table-column>
+      <el-table-column prop="rating" label="Rating" width="100"></el-table-column>
 
       <!-- Basic Media Information of Movie -->
-      <el-table-column prop="duration" label="Duration" width="200">
-        <template scope="scope">
-          {{getTimeString(scope.row.time)}}
-        </template>
-      </el-table-column>
+      <el-table-column prop="duration" label="Duration" width="200"></el-table-column>
 
       <!-- The information of the content of the Movie -->
       <el-table-column prop="genres" label="Genres" width="300">
-        <!-- TODO 多个按钮形式？或者文本用什么进行分隔？-->
+        <template scope="scope">
+          {{getCommaSplitGenres(scope.row.genres)}}
+        </template>
       </el-table-column>
       <!-- Plot - Ignored in List Mode -->
 
       <!-- Size (extends from Media) -->
-      <el-table-column prop="size" label="File Size" width="100">\
+      <el-table-column prop="size" label="File Size" width="100">
         <template scope="scope">
-          {{scope.row.size.size}}&nbsp;{{scope.row.size.measure}}
+          {{scope.row.size}}
         </template>
       </el-table-column>
 
       <!-- Available Operations -->
-      <el-table-column label="Operations">
+      <el-table-column label="Operations" width="300">
         <template scope="scope">
           <el-button type="primary" size="small" @click="goDetails(scope.row.id)">
             Details
@@ -50,26 +44,33 @@
 </template>
 
 <script>
-  import tableData from '@/assets/data';
-  import ElTable from "../../../node_modules/element-ui/packages/table/src/table";
+//  import ElTable from "../../../node_modules/element-ui/packages/table/src/table";
 
   export default {
-    components: {ElTable},
+//    components: {ElTable},
     beforeMount () {
       this.$axios.get('/api/movie').then(response => {
-        this.data = response.data.data;
+        window.console.log(response);
+        this.tableData = response.data.data;
       });
-    },
-    mounted () {
-      window.console.log(tableData);
     },
     data () {
       return {
-        tableData,
-        data: []
+        tableData: []
       };
     },
     methods: {
+      getCommaSplitGenres (genres) {
+        let str = "";
+        for (let i = 0; i < genres.length; ++i) {
+          if (i !== genres.length - 1) {
+            str += (genres[i] + ", ")
+          } else {
+            str += genres[i];
+          }
+        }
+        return str;
+      },
       getTimeString (time) {
         return `${Math.floor(time / 3600)} hrs ${Math.floor(
           (time % 3600) / 60)} mins ${(time % 3600) % 60} secs`;
@@ -94,9 +95,7 @@
       }
     },
     computed: {
-      filterTableData () {
-        return this.tableData.filter(element => element.type === 'Movies');
-      }
+      //
     }
   }
 </script>
